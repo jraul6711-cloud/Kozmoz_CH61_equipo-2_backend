310e4e4f81602d993f39296eb440184b256f0d84
Adrian Bringas <jraul6711@gmail.com>
2026-01-07 15:51:14 -0600

    Addition of files generated with spring initializer

---
+++ src/main/java/com/kozmoz/Kozmoz_CH61_equipo_2_backend/KozmozCh61Equipo2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.kozmoz.Kozmoz_CH61_equipo_2_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KozmozCh61Equipo2BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KozmozCh61Equipo2BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kozmoz/Kozmoz_CH61_equipo_2_backend/KozmozCh61Equipo2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kozmoz.Kozmoz_CH61_equipo_2_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KozmozCh61Equipo2BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
