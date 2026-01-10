fc4da5938212a85982e8f53f7fbb461603744ce8
Jessica B. Pérez <jessica26per@gmail.com>
2026-01-10 13:08:07 -0600

    Modelo, Repositorio y Controller de Libros primera parte

--- src/main/java/com/kozmoz/Kozmoz_CH61_equipo_2_backend/controller/LibroController.java
+++ src/main/java/com/kozmoz/Kozmoz_CH61_equipo_2_backend/controller/LibroController.java
@@ -1,12 +1,26 @@
 package com.kozmoz.Kozmoz_CH61_equipo_2_backend.controller;
 
+import java.util.List;
+
+import com.kozmoz.Kozmoz_CH61_equipo_2_backend.model.Libro;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 @RequestMapping(path="/api") 
 public class LibroController  {
 	//GET
+	@GetMapping("/libro/")
+	public List<Libro> getLibros(){
+		return List.of(
+				new Libro("nombre: Clean Code", "descripcion: Manual de manual de estilo para el desarrollo de software", "imagen.jpg", 850.00),
+				new Libro("nombre: El Hobbit", "descripcion: Una aventura épica en la Tierra Media", "imagenjpg", 420.00)
+				);
+		
+	}//getLibros 
 	
 	//POST
 	//DELETE
